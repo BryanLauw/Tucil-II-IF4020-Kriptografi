d77b0e98a4eca83473a3ac106fa12723ec9daf01
BryanLauw <13522033@std.stei.itb.ac.id>
2025-09-29 21:25:40 +0700

    add komentar di vigenere.py

--- src/Vigenere.py
+++ src/Vigenere.py
@@ -1,5 +1,25 @@
 def encrypt_vigenere(plain: int, key: int) -> int:
+    """
+    Mengenkripsi satu byte plain dengan satu byte key
+    
+    Args: 
+        plain (int): satu byte plaintext pada posisi ke-i
+        key (int): satu byte kunci pada posisi ke-(i % len(key asli)
+        
+    Output:
+        satu byte ciphertext
+    """
     return (plain + key) % 256
     
 def decrypt_vigenere(cipher: int, key: int) -> int:
+    """
+    Mendekripsi satu byte cipher dengan satu byte key
+    
+    Args: 
+        cipher (int): satu byte ciphertext pada posisi ke-i
+        key (int): satu byte kunci pada posisi ke-(i % len(key asli)
+        
+    Output:
+        satu byte plaintext
+    """
     return (cipher - key) % 256
